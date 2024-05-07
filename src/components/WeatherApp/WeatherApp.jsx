20d437f699382565070f70ca4c83d8fd63108efb
Jemster07 <emelie.kerek@gmail.com>
2024-05-07 15:19:02 -0400

    got weather icon loading in the background

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -84,62 +84,66 @@ const WeatherApp = () => {
     }
 
     return (
-        <div className="app">
+        <div>
             {initialVisit ?
-                <div className="landing-page">
-                    <div className="search-field">
-                        {location === undefined ?
-                            <button disabled className="current-location-button">
-                                <div className="loader"></div>
-                            </button>
-                        :
-                            <button className="current-location-button" onClick={permissionCheck}>
-                                <p>Use Current Location</p>
-                            </button>
-                        }                       
+                <div className="landing">
+                    <div className="container">
+                        <div className="search-field">
+                            {location === undefined ?
+                                <button disabled className="current-location-button">
+                                    <div className="loader"></div>
+                                </button>
+                            :
+                                <button className="current-location-button" onClick={permissionCheck}>
+                                    <p>Use Current Location</p>
+                                </button>
+                            }                       
 
-                        <div className="prompt-text">- OR -</div>
+                            <div className="prompt-text">- OR -</div>
 
-                        <form onSubmit={handleSubmit}>
-                            <input className="zipInput" type="text" value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} pattern="[0-9]{5}" placeholder="ZIP code search" required />
-                            <button className="search-button" type="submit">
-                                <img src={search_icon} alt="search button" />
-                            </button>
-                        </form>
+                            <form onSubmit={handleSubmit}>
+                                <input className="zipInput" type="text" value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} pattern="[0-9]{5}" placeholder="ZIP code search" required />
+                                <button className="search-button" type="submit">
+                                    <img src={search_icon} alt="search button" />
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             :                   
-                <div className="result">
-                    <figure className="weather-image">
-                        <img src={weatherIcon} alt="" />
-                    </figure>
-                    <div className="weather-temp">{temp}&deg;F</div>
-                    <div className="weather-location">{location}</div>
-                    <div className="data-container">
-                        <div className="element">
-                            <img src={humidity_icon} alt="" className="icon" />
-                            <div className="data">
-                                <div className="humidity-percent">{humidity}%</div>
-                                <div className="text">Humidity</div>
+                <div className="result" style={{backgroundImage: "url(" + weatherIcon + ")"}}>
+                    <div className="container">
+                        <figure className="weather-image">
+                            <img src={weatherIcon} alt="" />
+                        </figure>
+                        <div className="weather-temp">{temp}&deg;F</div>
+                        <div className="weather-location">{location}</div>
+                        <div className="data-container">
+                            <div className="element">
+                                <img src={humidity_icon} alt="" className="icon" />
+                                <div className="data">
+                                    <div className="humidity-percent">{humidity}%</div>
+                                    <div className="text">Humidity</div>
+                                </div>
+                            </div>
+                            <div className="element">
+                                <img src={wind_icon} alt="" className="icon" />
+                                <div className="data">
+                                    <div className="wind-rate">{wind} mph</div>
+                                    <div className="text">Wind Speed</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="element">
-                            <img src={wind_icon} alt="" className="icon" />
-                            <div className="data">
-                                <div className="wind-rate">{wind} mph</div>
-                                <div className="text">Wind Speed</div>
-                            </div>
+                        <div className="search-field">
+                            <form onSubmit={handleSubmit}>
+                                <input className="zipInput" type="text" value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} pattern="[0-9]{5}" placeholder="ZIP code search" required />
+                                <button className="search-button" type="submit">
+                                    <img src={search_icon} alt="search button" />
+                                </button>
+                            </form>
                         </div>
-                    </div>
-                    <div className="search-field">
-                        <form onSubmit={handleSubmit}>
-                            <input className="zipInput" type="text" value={searchQuery} onChange={(event) => setSearchQuery(event.target.value)} pattern="[0-9]{5}" placeholder="ZIP code search" required />
-                            <button className="search-button" type="submit">
-                                <img src={search_icon} alt="search button" />
-                            </button>
-                        </form>
-                    </div>
-                </div>           
+                    </div>    
+                </div>       
             }           
         </div>
     )
